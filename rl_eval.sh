da65dde912a8ad66bea01c2ca4b019c29c9a3b95
Sam Earle <se2161@log-1.nyu.cluster>
2021-06-06 19:30:06 -0400

    Fiddle with sbatch in a futile manner.

--- rl_eval.sh
+++ rl_eval.sh
@@ -1,25 +1,23 @@
 #!/bin/bash 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=12
 
 ## We won't be asking for gpus, for now
 ##SBATCH --gres=gpu:1
 
 #SBATCH --time=12:00:00
-#SBATCH --mem=30GB
+#SBATCH --mem=64GB
 #SBATCH --job-name=pcgrl
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=sam.earle@nyu.edu
 #SBATCH --output=pcgrl_%j.out
 
-cd /scratch/se2161/evo-pcgrl
+cd /scratch/se2161/evo-pcgrl || exit
 
-## Is this actually necessary?
 source activate vanilla_pcgrl
-
 ## NOTE THIS ACTUALLY WORKS DONT LISTEN TO THE ERROR MESSAGE ???
 conda activate vanilla_pcgrl
 
-python evaluate_ctrl.py -la 11
+python evaluate_ctrl.py -la 26
 
