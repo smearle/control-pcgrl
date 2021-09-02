bea7f1b48482e9fd73c379d862d18c2c52f7f096
Sam Earle <se2161@log-1.nyu.cluster>
2021-09-02 17:20:48 -0400

    sbatch fix

--- evo_train.sh
+++ evo_train.sh
@@ -22,7 +22,7 @@ source activate
 conda activate evo-pcgrl
 
 start=$SECONDS
-while ! python train_ctrl.py -la 0
+while ! python evolve.py -la 0
 do
     duration=$((( SECONDS - start ) / 60))
     echo "Script returned error after $duration minutes"
