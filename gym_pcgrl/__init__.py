320ff33d19ab32449d7cae0e492a52492de4807e
JiangZehua <jiangzehua_work@foxmail.com>
2022-02-04 19:53:08 -0500

    update the env register code of seperating 2D and 3D envs

--- gym_pcgrl/__init__.py
+++ gym_pcgrl/__init__.py
@@ -4,19 +4,18 @@ from gym_pcgrl.envs.reps import REPRESENTATIONS
 
 # Register all the problems with every different representation for the OpenAI GYM
 for prob in PROBLEMS.keys():
-    if prob == "minecraft_3D_maze":
-        continue
     for rep in REPRESENTATIONS.keys():
-        if rep == "narrow3D":
+        if ("3D" not in prob) and ("3D" not in rep):
+            register(
+                id='{}-{}-v0'.format(prob, rep),
+                entry_point='gym_pcgrl.envs:PcgrlEnv',
+                kwargs={"prob": prob, "rep": rep}
+            )
+        elif ("3D" in prob) and ("3D" in rep):
+            register(
+                id='{}-{}-v0'.format(prob, rep),
+                entry_point="gym_pcgrl.envs:PcgrlEnv3D",
+                kwargs={"prob": prob, "rep": rep}
+            )
+        else:
             continue
-        register(
-            id='{}-{}-v0'.format(prob, rep),
-            entry_point='gym_pcgrl.envs:PcgrlEnv',
-            kwargs={"prob": prob, "rep": rep}
-        )
-
-register(
-    id="minecraft_3D_maze-narrow3D-v0",
-    entry_point="gym_pcgrl.envs:PcgrlEnv3D",
-    kwargs={"prob": "minecraft_3D_maze", "rep": "narrow3D"}
-)
