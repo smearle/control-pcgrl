Bugfix: the calculation in path coords are fixed now.
Note that in 2D helper function, the coords fed into `get_path_coords`
need to be flipped. i.e. (y, x)
But in 3D cases, it's fine to input the regular order. i.e. (x, y, z)

gym_pcgrl/gym_pcgrl/envs/probs/zelda/zelda_prob.py
@@ -124,8 +124,8 @@ class ZeldaProblem(Problem):
                 map_stats["path-length"] += dijkstra_d[d_y][d_x]
                 if self.render_path:
                                                                              # end point is key
-                    self.path = np.hstack((get_path_coords(dijkstra_k, init_coords=(k_x, k_y)),
-                                          get_path_coords(dijkstra_d, init_coords=(d_x, d_y))))
+                    self.path = np.hstack((get_path_coords(dijkstra_k, init_coords=(k_y, k_x)),
+                                          get_path_coords(dijkstra_d, init_coords=(d_y, d_x))))
                                                                              # end point is door
         self.path_length = map_stats["path-length"]
         return map_stats