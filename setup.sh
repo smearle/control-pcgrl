e411633d41f7bfffdaac73f7211b5c4e6a4573af
Zehua Jiang <zj2086@nyu.edu>
2022-10-13 15:23:32 -0400

    update requirements etc

--- setup.sh
+++ setup.sh
@@ -16,6 +16,9 @@ conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 # for 3090
 # conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
 
+# # M1 mac users, if want to use GPU (MPS acceleration)):
+# conda install pytorch torchvision torchaudio -c pytorch-nightly
+
 
 # GUI libraries for rendering (and controlling) controllable agents.
 conda install -c conda-forge pygobject gtk3
